cc4594622bb6e497ae5a0f2e8fd22636218c5417
Doofitator <ashley@sharkey.id.au>
2019-06-09 15:20:17 +1000

    Fixed stuffups

--- Marist Chat App/testPages/Default.aspx.vb
+++ Marist Chat App/testPages/Default.aspx.vb
@@ -4,4 +4,16 @@ Partial Class testPages_Default
     Protected Sub Page_PreInit(sender As Object, e As EventArgs) Handles Me.PreInit
         If Request.Browser.IsMobileDevice Then MasterPageFile = "~/MobileMasterPage.master"
     End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButtonList1.SelectedIndex = 0 Then
+            TextBox1.Text = "You selected Test1"
+        ElseIf RadioButtonList1.SelectedIndex = 1 Then
+            TextBox1.Text = "You selected Test2"
+        ElseIf RadioButtonList1.SelectedIndex = 2 Then
+            TextBox1.Text = "You selected Test3"
+        Else
+            TextBox1.Text = "You didn't select anything."
+        End If
+    End Sub
 End Class
