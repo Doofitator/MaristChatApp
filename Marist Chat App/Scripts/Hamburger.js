10bbe17f32cc98b4235248a330ae1bc6e62b873a
Doofitator <ashley@sharkey.id.au>
2019-06-09 15:10:13 +1000

    Got working mobile master

---
+++ Marist Chat App/Scripts/Hamburger.js
@@ -0,0 +1,9 @@
+﻿function hamburger(x) {
+    x.classList.toggle("change");
+    var menuIcon = document.getElementById('menuIcon');
+    if (menuIcon.style.display == "block") {
+        menuIcon.style.display = "none";
+    } else {
+        menuIcon.style.display = "block";
+    }
+}
