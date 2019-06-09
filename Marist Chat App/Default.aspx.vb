e0ca5497f92f1a1692e1a7840d7099c0f952a519
Doofitator <ashley@sharkey.id.au>
2019-06-09 13:31:00 +1000

    Added mobile master page

--- Marist Chat App/Default.aspx.vb
+++ Marist Chat App/Default.aspx.vb
@@ -2,4 +2,8 @@
 Partial Class _Default
     Inherits System.Web.UI.Page
 
+
+    Protected Sub Page_PreInit(sender As Object, e As EventArgs) Handles Me.PreInit
+        If Request.Browser.IsMobileDevice Then MasterPageFile = "~/MobileMasterPage.master"
+    End Sub
 End Class
---
+++ Marist Chat App/MobileMasterPage.master.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class MobileMasterPage
+    Inherits System.Web.UI.MasterPage
+End Class
+
