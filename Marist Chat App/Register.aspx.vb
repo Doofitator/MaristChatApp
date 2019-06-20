72f9d7e1962046235ab7ccaadff128db39f8f674
Doofitator <ashley@sharkey.id.au>
2019-06-20 15:18:56 +1000

    Signup page that doesn't do anything

--- Marist Chat App/Register.aspx.vb
+++ Marist Chat App/Register.aspx.vb
@@ -1,12 +1,13 @@
 ﻿
-Partial Class _Default
+Partial Class Register
     Inherits System.Web.UI.Page
 
     Protected Sub Page_PreInit(sender As Object, e As EventArgs) Handles Me.PreInit
         'check if device is mobile & set master page accordingly
         If Request.Browser.IsMobileDevice Then MasterPageFile = "~/MobileMasterPage.master"
-        'ensure user is authenticated
-        If Not User.Identity.IsAuthenticated Then
+
+        'check that the user isn't here a second time accidentally
+        If User.Identity.IsAuthenticated Then
             Response.Redirect("/Default.aspx")
         End If
     End Sub
