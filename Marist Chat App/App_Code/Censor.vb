99e3c962ca7c85608a9b8fb67d7dc83cd531f7ed
Doofitator <ashley@sharkey.id.au>
2019-08-14 18:19:02 +1000

    documenting again

--- Marist Chat App/App_Code/Censor.vb
+++ Marist Chat App/App_Code/Censor.vb
@@ -1,29 +1,36 @@
 ﻿Imports Microsoft.VisualBasic
 Public Class Censor
     Public Shared CensoredWords As Generic.IList(Of String)
-    Public Sub New()
+    Public Sub New() 'New instance of censor class
+        'Throw error if we can't find the bad words
         If CensoredWords Is Nothing Then Throw New ArgumentNullException("censoredWords")
+
+        'Reset the censored words
         CensoredWords = New Generic.List(Of String)(CensoredWords)
     End Sub
 
-    Public Function CensorText(ByVal text As String) As String
-        If text Is Nothing Then Throw New ArgumentNullException("text")
-        Dim censoredText As String = text
+    Public Function CensorText(ByVal strInput As String) As String
 
-        For Each censoredWord As String In CensoredWords
-            Dim regularExpression As String = ToRegexPattern(censoredWord)
-            censoredText = Regex.Replace(censoredText, regularExpression, AddressOf StarCensoredMatch, RegexOptions.IgnoreCase Or RegexOptions.CultureInvariant)
+        'If theres no text to censor then throw an error
+        If strInput Is Nothing Then Throw New ArgumentNullException("text")
+        Dim strCensoredText As String = strInput 'output
+
+        For Each censoredWord As String In CensoredWords 'for each bad word we have
+            Dim regularExpression As String = ToRegexPattern(censoredWord) 'Make a new regular expression to match it
+            strCensoredText = Regex.Replace(strCensoredText, regularExpression, AddressOf StarCensoredMatch, RegexOptions.IgnoreCase Or RegexOptions.CultureInvariant) 'call StarCensoredMatch if we find the regex in the string
         Next
 
-        Return censoredText
+        Return strCensoredText
     End Function
 
     Private Shared Function StarCensoredMatch(ByVal m As Match) As String
-        Dim word As String = m.Captures(0).Value
-        Return New String("*"c, word.Length)
+        Dim word As String = m.Captures(0).Value    '| Replace each bad letter with
+        Return New String("*"c, word.Length)        '| a star character
     End Function
 
     Private Function ToRegexPattern(ByVal wildcardSearch As String) As String
+        'Convert out bad word to a regex search pattern
+
         Dim regexPattern As String = Regex.Escape(wildcardSearch)
         regexPattern = regexPattern.Replace("\*", ".*?")
         regexPattern = regexPattern.Replace("\?", ".")
